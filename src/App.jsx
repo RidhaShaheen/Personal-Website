b71a1c7608a264506cb66ba480b866f127dc15c7
RidhaShaheen <ridhashaheen.s@gamil.com>
2025-04-02 14:13:37 -0400

    imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import styles from './App.module.css'
 import Landing from "./pages/Landing/Landing"
-import AboutMe  from './pages/About/aboutMe'
+import AboutMe  from './pages/About/AboutMe'
 import Experience from './pages/Experience/Experience'
 import {Navbar} from "./componets/Navbar/Navbar"
 import Education from './pages/Education/Education'
